9b6106ac84d3b4770418174c21366399a35290c2
louisb0 <louisbaragwanath@outlook.com>
2024-05-22 21:41:40 +1200

    Add evaluation to REPL

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.lexer import Lexer
 from src.parser import Parser
 import src.ast as ast
+import src.evaluator as evaluator
 
 PROMPT = ">> "
 
@@ -10,9 +11,13 @@ def main():
     print("Prefix your expression with 'visualise: ' to get a rendered AST.")
     print("=" * 60)
 
-    expression = input(PROMPT)
-    while expression:
+    while True:
+        expression = input(PROMPT)
+        if not expression:
+            return
+
         visualise_ast = "visualise: " in expression.lower()
+        expression = expression.replace("visualise: ", "")
 
         lexer = Lexer(expression)
         parser = Parser(lexer)
@@ -20,14 +25,14 @@ def main():
 
         if len(parser.errors) != 0:
             print("\n".join(parser.errors))
-        else:
-            print(program)
-            if visualise_ast:
-                graph = ast.ast_to_dot(program)
-                graph.render("ast", format="png", view=True)
+            continue
 
-        expression = input(PROMPT)
+        if visualise_ast:
+            graph = ast.ast_to_dot(program)
+            graph.render("ast", format="png", view=True)
 
+        evaluated = evaluator.eval(program)
+        print(evaluated)
 
 if __name__ == "__main__":
     main()
