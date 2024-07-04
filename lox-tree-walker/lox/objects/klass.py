0bee771215f2211c8f46b09928d597a7a5c48a84
louisb0 <louisbaragwanath@outlook.com>
2024-07-04 20:30:33 +1200

    Setup basics of class instances

--- lox-tree-walker/lox/objects/klass.py
+++ lox-tree-walker/lox/objects/klass.py
@@ -1,6 +1,28 @@
-class Class:
+from typing import TYPE_CHECKING, Any
+
+if TYPE_CHECKING:
+    from lox.interpreter import Interpreter
+
+from lox.objects.callables import Callable
+
+
+class Class(Callable):
     def __init__(self, name: str):
         self.name = name
 
+    def call(self, interpreter: "Interpreter", arguments: list) -> Any:
+        return Instance(self)
+
+    def arity(self) -> int:
+        return 0
+
     def __str__(self) -> str:
         return self.name
+
+
+class Instance:
+    def __init__(self, klass: "Class"):
+        self.klass = klass
+
+    def __str__(self) -> str:
+        return f"{self.klass} instance"
