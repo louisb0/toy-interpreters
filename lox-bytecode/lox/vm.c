ad5fa48ff8fc4309b269040e3fc4434457415473
louisb0 <louisbaragwanath@outlook.com>
2024-07-08 17:34:44 +1200

    Implement VM operand stack

--- lox-bytecode/lox/vm.c
+++ lox-bytecode/lox/vm.c
@@ -6,8 +6,21 @@
 
 static VM vm;
 
-void init_vm() {}
+static void reset_stack() { vm.stack_top = vm.stack; }
+
+void init_vm() { reset_stack(); }
+
 void free_vm() {}
+
+void push(Value value) {
+  *vm.stack_top = value;
+  vm.stack_top++;
+}
+
+Value pop() {
+  vm.stack_top--;
+  return *vm.stack_top;
+}
 
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
@@ -15,18 +28,25 @@ static InterpretResult run() {
 
   for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
+    printf("          ");
+    for (Value *slot = vm.stack; slot < vm.stack_top; slot++) {
+      printf("[ ");
+      print_value(*slot);
+      printf(" ]");
+    }
+    printf("\n");
     dissassemble_instruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
 #endif
 
     uint8_t instruction;
     switch (instruction = READ_BYTE()) {
     case OP_CONSTANT: {
-      Value constant = READ_CONSTANT();
-      print_value(constant);
-      printf("\n");
+      push(READ_CONSTANT());
       break;
     }
     case OP_RETURN:
+      print_value(pop());
+      printf("\n");
       return INTERPRET_OK;
     }
   }
